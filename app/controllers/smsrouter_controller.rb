6dfd5e484231a14eab325030104c5864a0f5ec9a
Bin Zhu <binzhu1070@gmail.com>
2012-09-10 23:30:42 -0700

    direction and weather done

--- app/controllers/smsrouter_controller.rb
+++ app/controllers/smsrouter_controller.rb
@@ -9,23 +9,35 @@ class SmsrouterController < ApplicationController
       from_number = params[:From]
       sms_in_body = params[:Body]
       #puts sms_in_body
-      type = routesms(sms_in_body)[:type]
+      type = routesms(sms_in_body)["type"]
+      
+      puts "debug info"
+      puts sms_in_body
+      puts routesms sms_in_body
       puts type
       
       if type == "direction"
-        msgBack = sms_in_body
+        msgBack = google_direction(routesms(sms_in_body)["from"],routesms(sms_in_body)["to"])
       elsif type == "weather"
-        msgBack = weatherMsg(routesms(sms_in_body)[:q])
+        msgBack = weatherMsg(routesms(sms_in_body)["q"])
       elsif type == "error"
-        msgBack = routesms(sms_in_body)[:helpMsg]
+        msgBack = routesms(sms_in_body)["helpMsg"]
       else
       end
+      #@data = weatherMsg(routesms(sms_in_body)["q"])
+      #puts msgBack
+      count = msgBack.length/160 
       
-      @client.account.sms.messages.create(
-        :from => "+13158951310",
-        :to => from_number,
-        :body => msgBack
-      )
+          while count > 0 
+            @client.account.sms.messages.create(
+             :from => +13158951310,
+             :to => from_number,
+             :body => msgBack.first(160)
+            )
+            msgBack = msgBack[160, msgBack.length - 1]
+          
+          sleep 1
+          end
     end
   end
   
@@ -60,25 +72,25 @@ class SmsrouterController < ApplicationController
                           #puts base_arr[a] +" "+ loca_1
                           #puts base_arr[b] +" "+ loca_2
                           {
-                            "type" => command_in,
+                            "type" => "direction",
                             base_arr[a] => loca_1,
                             base_arr[b] => loca_2
                           }
-                          #puts result.inspect
+            
                           
-                  #recognize and respond to weather request
-                  elsif weather_command.include?(command_in)
-                    # check if the first word after command is prepsition
+          #recognize and respond to weather request
+          elsif weather_command.include?(command_in)
+          # check if the first word after command is prepsition
                     
-                    if base_arr[1] =~ /at|of|in|to/
-                      weather_query = base_arr[2..base_arr.length-1].join(" ")
-                    else
-                      weather_query = base_arr[1..base_arr.length-1].join(" ")
-                    end
+            if base_arr[1] =~ /at|of|in|to/
+              weather_query = base_arr[2..base_arr.length-1].join(" ")
+            else
+              weather_query = base_arr[1..base_arr.length-1].join(" ")
+            end
                           
-                            {"type"	=> command_in,
-                              "q" 	=> weather_query}
-                  end
+                     {"type"	=> "weather",
+                      "q" 	=> weather_query}
+            end
                   
           #system didn't find a corresponding command
           else  
@@ -91,20 +103,52 @@ class SmsrouterController < ApplicationController
   end  #routesms(sms_in)
   
   def google_direction(origin,destiny)
+    direction_uri = "http://maps.googleapis.com/maps/api/directions/json?origin=" + origin.gsub(" ","+") + "&destination=" + destiny.gsub(" ","+") + "&sensor=false"
+    response = ""
     
+    json_resp = (JSON.parse(Typhoeus::Request.get(direction_uri).body))["routes"][0]["legs"][0]["steps"]
+    i = 0
+    json_resp.each do |leg|
+      i += 1
+      response += leg["html_instructions"]
+      response += (i<json_resp.count)? ", " : "." 
+    end
+    response.gsub("<b>","").gsub("</b>","").gsub('<div style="font-size:0.9em">',' ').gsub("</div>","")
   end
   
   def weatherMsg(q)
-    weather_url = "http://free.worldweatheronline.com/feed/weather.ashx"
+    weather_uri = "http://free.worldweatheronline.com/feed/weather.ashx"
     query = "&format=json&num_of_days=2&key=" + ENV['weather_token']
-    api_call_url = weather_url + "?q=" + q + query
-    response = Typhoeus::Request.get api_call_url
-    puts response.inspect
+    api_call_uri = weather_uri + "?q=" + q + query
+    json_resp = (JSON.parse(Typhoeus::Request.get(api_call_uri).body))["data"]
+    #puts json_resp.flatten.inspect
+    #puts "br"
+    #puts json_resp["current_condition"].inspect
+    #puts "br"
+    #puts json_resp["weather"].inspect
+    #puts "br"
+    #puts json_resp["request"].inspect
+      
+    cur_temp = json_resp["current_condition"][0]["temp_C"] + "C/" + json_resp["current_condition"][0]["temp_F"] + "F"
+    cur_desc = json_resp["current_condition"][0]["weatherDesc"][0]["value"].downcase
     
-    temp_c = response[:data][:current_condition][:temm_C]
-    temp_f = response[:data][:current_condition][:temm_F]
+    today_low =   json_resp["weather"][0]["tempMinC"] + "C/" + json_resp["weather"][0]["tempMinF"] + "F"
+    today_high =  json_resp["weather"][0]["tempMaxC"] + "C/" + json_resp["weather"][0]["tempMaxF"] + "F"
+    today_desc = json_resp["weather"][0]["weatherDesc"][0]["value"].downcase
     
-    "current temperature is: " + temp_c + "C/" + temp_f + "F"
+    tom_low =     json_resp["weather"][1]["tempMinC"] + "C/" + json_resp["weather"][0]["tempMinF"] + "F"
+    tom_high =    json_resp["weather"][1]["tempMaxC"] + "C/" + json_resp["weather"][0]["tempMaxF"] + "F"
+    tom_desc = json_resp["weather"][1]["weatherDesc"][0]["value"].downcase
     
+    #final msg to send back
+    "Current: "+ cur_desc + ", " + cur_temp +  ", Today: " + today_desc + ", " + today_low + "~" + today_high +  ", Tomorrow: " + tom_desc + ", " + tom_low + "~" + tom_high 
+  end
+  
+  def sendmsg(msg)
+    if msg.length<=140
+      msg
+    else
+      msglist = []
+    end
   end
 end
